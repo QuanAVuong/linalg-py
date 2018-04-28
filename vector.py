30fb7f771c0e5e6cc5e15048641f20526003f7a2
Quan <QuanAVuong@users.noreply.github.com>
2018-04-28 00:01:36 -0400

    Vector - __init__(): sets the dimension of space that the vector lives in

--- vector.py
+++ vector.py
@@ -1,2 +1,7 @@
 class Vector(object):
   def __init__(self, coordinates):
+    try:
+      if not coordinates:
+        raise ValueError
+      self.coordinates = tuple(coordinates)
+      self.dimension = len(coordinates)
